ab0a24179dbed52d2aa4cd21a8b28d86c4b1ca8d
Abdul Salam <desperateapps@gmail.com>
2024-12-21 20:23:18 +0800

    Initial Commit

---
+++ BIMBWeatherAssignment/BIMBWeatherAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIMBWeatherAssignmentApp.swift
+//  BIMBWeatherAssignment
+//
+//  Created by Abdul Salam on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BIMBWeatherAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
